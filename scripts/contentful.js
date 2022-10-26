768790ae6e3e1181b90cedd9f9e4217ff28dfaf7
Bianca Blakesley <biancablakesley@biancas-mbp.mynetworksettings.com>
2022-10-26 11:56:47 -0400

    config change - env variable test

--- scripts/contentful.js
+++ scripts/contentful.js
@@ -1,7 +1,7 @@
-const spaceId = config.SPACEID;
+const spaceId = SPACEID;
 const spaceName = "Uova";
 const environmentId = "master"
-const accessToken = config.ACCESSTOKEN;
+const accessToken = ACCESSTOKEN;
 const url = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}&order=fields.order&content_type=menuItems`
 
 const sectionTag = document.querySelector('section.grid')
